7a6ae8d28d186b340bc8b93b1a2d5e2b3aa49d55
nvk3d <nike_bondar@mail.ru>
2017-05-16 11:17:14 +0300

    init repo & added new classes

---
+++ src/com/random/Main.java
@@ -0,0 +1,18 @@
+package com.random;
+
+import java.util.Scanner;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+	    new Main().start();
+    }
+
+    public void start() {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        System.out.println(str);
+    }
+
+}
---
+++ src/com/random/common/Calendar.java
@@ -0,0 +1,7 @@
+package com.random.common;
+
+/**
+ * Created by nvk3d on 16.05.17.
+ */
+public class Calendar {
+}
